1a28010da6c1192d3c40a93af0a07e6144e3af52
Mark Johnson <mjohnson@10thmagnitude.com>
2017-01-30 02:24:45 -0600

    adding webapi routing

---
+++ aspnet-get-started/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace aspnet_get_started
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services 
+
+			// Web API routes 
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(name: "DefaultApi",
+										routeTemplate: "api/{controller}/{id}",
+										defaults: new
+												{
+													id = RouteParameter.Optional
+												});
+		}
+	}
+}
